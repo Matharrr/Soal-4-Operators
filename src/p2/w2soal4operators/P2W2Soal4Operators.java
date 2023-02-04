311c123fc5526681be7c6b9db45fae6ca1ffc391
Matharrr <117796799+Matharrr@users.noreply.github.com>
2023-02-05 00:00:50 +0700

    Upload

---
+++ src/p2/w2soal4operators/P2W2Soal4Operators.java
@@ -0,0 +1,13 @@
+package p2.w2soal4operators;
+/**
+ *
+ * @author Muhamad Mathar Rizqi
+ */
+public class P2W2Soal4Operators {
+    public static void main(String[] args) {
+    double x = 92.98;
+    int nx = (int) Math.round(x);
+    System.out.println(nx);
+    }
+}
+
